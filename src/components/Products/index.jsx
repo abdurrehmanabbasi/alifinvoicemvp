8cf09f9964293fd00336b847fd49ead3d134602e
abdurrehman <abdurrehmanabbasi8@gmail.com>
2023-05-26 23:42:08 +0500

    Product Part#1

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,5 +1,14 @@
+import {doc, getDoc} from "firebase/firestore"
+import {db} from "../../app/firebase"
+import useAuth from "../../hooks/useAuth"
+import { useEffect, useState } from "react"
 const Products = () => {
-    return ( <>Products</> );
+    
+
+    return ( <div>
+        <h2>Product Managment </h2>
+
+    </div> );
 }
  
 export default Products;
