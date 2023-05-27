5987bdea3a64ddf4db633ec8a38a25294f38c4f9
abdurrehman <abdurrehmanabbasi8@gmail.com>
2023-05-27 11:58:13 +0500

    Make Responsive and Add New Product Form

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,18 +1,31 @@
 import { Navigate, Outlet } from "react-router-dom";
-import useAuth from "../hooks/useAuth"
+import useAuth from "../hooks/useAuth";
 import Sidebar from "../Sidebar";
-import { useEffect } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
 const Layout = () => {
-    const {user}=useAuth()
-    useEffect(()=>{
-        document.title="Alif Invoices"
-    },[])
-    return (
-    !user?<Navigate to={"/auth"}/>:        
-        <div className="app flex w-screen h-screen overflow-hidden">
-            <Sidebar/>
-        <main className="overflow-y-auto"><Outlet/></main>
-    </div> );
-}
- 
+  const [openMenu, setOpenMenu] = useState(true);
+  const { user } = useAuth();
+  useEffect(() => {
+    document.title = "Alif Invoices";
+  }, []);
+  return !user ? (
+    <Navigate to={"/auth"} />
+  ) : (
+    <div className="app flex w-screen h-screen overflow-hidden">
+      <button onClick={() => setOpenMenu(!openMenu)} className="p-3 absolute top-0 left-0 ">
+        {!openMenu ? (
+          <Bars3Icon className="w-8" />
+        ) : (
+          <XMarkIcon className="w-8" />
+        )}{" "}
+      </button>
+      <Sidebar openMenu={openMenu} setOpenMenu={setOpenMenu} />
+      <main className="min-w-full ml-10 overflow-y-auto">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
 export default Layout;
