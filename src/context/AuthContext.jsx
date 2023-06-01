93da75cf1a1ce96cbc385e7701ba82b616bc2eb4
abdurrehman <abdurrehmanabbasi8@gmail.com>
2023-06-01 06:08:54 +0500

    update Authcontext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,14 +1,12 @@
 import { createContext, useEffect, useState } from "react";
-import { auth, db } from "../app/firebase";
+import { auth} from "../app/firebase";
 import { onAuthStateChanged } from "firebase/auth";
-import { doc,getDoc } from "firebase/firestore";
 // Create AuthContext
 const AuthContext = createContext();
 
 // AuthProvider component
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [userDoc,setUserDoc]=useState(null)
   const [loading, setLoading] = useState(true);
 
   // Listen for user changes on component mount
@@ -16,17 +14,7 @@ const AuthProvider = ({ children }) => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       setUser(user);
       setLoading(false);
-      if (user) {
-        const docRef = doc(db, "users", user.uid);
-        const docSnap = await getDoc(docRef);
-
-        if (docSnap.exists()) {
-          setUserDoc(docSnap.data());
-        } else {
-          // docSnap.data() will be undefined in this case
-          setUserDoc(null)
-        }
-      }
+      
     });
 
     // Cleanup subscription on component unmount
@@ -37,7 +25,6 @@ const AuthProvider = ({ children }) => {
     <AuthContext.Provider
       value={{
         user,
-        userDoc
       }}
     >
       {loading ? <div>loading...</div> : children}
