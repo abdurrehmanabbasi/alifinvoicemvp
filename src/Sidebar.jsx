8fdddacb894d0fa3c39b663d40048f45585cfeec
abdurrehman <abdurrehmanabbasi8@gmail.com>
2023-08-28 03:55:57 +0500

    Dashboard Added

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -28,7 +28,11 @@ const Sidebar = () => {
               alt=""
             />
 
-            <p>{openMenu?'AQ khajoor Shop':'AQ'}</p>
+            <p>
+              {openMenu?<p>AQ</p>
+            :  <p>AQ</p>
+            }
+            </p>
           </div>
         </div>
 
@@ -37,47 +41,47 @@ const Sidebar = () => {
             className="flex p-4 gap-x-2 items-center"
             to="/app"
           >
-            <ChartBarIcon className="w-6" /><p className={`${openMenu?'':'hidden'}`}>Dashboard</p>
+            <ChartBarIcon className="w-6" /><p className={`${openMenu?'hidden md:block':' hidden'}`}>Dashboard</p>
           </NavLink>
           <NavLink
             className="flex hover:bg-orange-200 p-4 gap-x-2 items-center"
             to="products"
           >
             <Squares2X2Icon className="w-6" />
-            <p className={`${openMenu?'':'hidden'}`}>Products</p>
+            <p className={`${openMenu?'hidden md:block':' hidden'}`}>Products</p>
           </NavLink>
           <NavLink
             className="flex hover:bg-orange-200 p-4 gap-x-2 items-center"
             to="invoices"
           >
             <RectangleStackIcon className="w-6" />
-            <p className={`${openMenu?'':'hidden'}`}>Invoices</p>
+            <p className={`${openMenu?'hidden md:block':' hidden'}`}>Invoices</p>
           </NavLink>
           <NavLink
             className="flex hover:bg-orange-200 p-4 gap-x-2 items-center"
             to="customers"
           >
             <UserGroupIcon className="w-6" />
-            <p className={`${openMenu?'':'hidden'}`}>Customers</p>
+            <p className={`${openMenu?'hidden md:block':' hidden'}`}>Customers</p>
           </NavLink>
           <NavLink
             className="flex hover:bg-orange-200 p-4 gap-x-2 items-center"
             to="reports"
           >
             <DocumentChartBarIcon className="w-6" />
-            <p className={`${openMenu?'':'hidden'}`}>Reports</p>
+            <p className={`${openMenu?'hidden md:block':' hidden'}`}>Reports</p>
           </NavLink>
           <NavLink
             className="flex hover:bg-orange-200 p-4 gap-x-2 items-center"
             to="settings"
           >
             <Cog8ToothIcon className="w-6" />
-            <p className={`${openMenu?'':'hidden'}`}>Settings</p>
+            <p className={`${openMenu?'hidden md:block':' hidden'}`}>Settings</p>
           </NavLink>
         </nav>
       </div>
       {openMenu?
-      <div className="company p-2">
+      <div className="hidden md:block company p-2">
         <p className="">Powered By</p>
         <h2 className="text-orange-600 text-4xl font-bold">Alif Invoice</h2>
       </div>
